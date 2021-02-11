a82aff58fdcb0718239d14a98335ad38cc02cd2b
iEPCBM <rishatk87@gmail.com>
2021-02-11 21:42:51 +0300

    Update

--- calc.js
+++ calc.js
@@ -1,3 +1,35 @@
+function getVector(a, b) {
+  let retVal = []
+  if (a.length!===b.length) {
+    throw "getVector: точки a и b имеют разные размерности.";
+  }
+  for (var i = 0; i < a.length; i++) {
+    retVal.push(b[i]-a[i]);
+  }
+  return retVal;
+}
+
+function getNormalOfPlain(a, b, c) {
+  let matrixPart = [];
+  let vecAB, vecAC;
+  let normal = [];
+  try {
+    vecAB = getVector(a, b);
+    vecAC = getVector(a, c);
+  } catch (e) {
+    console.log(e);
+  }
+  matrixPart.push(vecAB);
+  matrixPart.push(vecAC);
+  // i  j k
+  // 0  1 2 - 0
+  // 0  1 2 - 1
+  normal.push(matrixPart[0][1]*matrixPart[1][2]-matrixPart[1][1]*matrixPart[0][2]); //i
+  normal.push(matrixPart[0][2]*matrixPart[1][0]-matrixPart[0][0]*matrixPart[1][2]); //j
+  normal.push(matrixPart[0][0]*matrixPart[1][1]-matrixPart[1][0]*matrixPart[0][1]); //k
+  return normal;
+}
+
 $(document).ready(function() {
   var martix_inputs = [
     [$("#A_X"), $("#A_Y"), $("#A_Z")],
@@ -15,9 +47,25 @@ $(document).ready(function() {
     });
     return true;
   };
+  function extractPoints(inputs) {
+    let retVal = [];
+    inputs.forEach((point, i) => {
+      retVal.push([]);
+      point.forEach((item, j) => {
+        retVal[i].push(parseInt(item.val()));
+      });
+    });
+    console.log(retVal);
+    return retVal;
+  }
   $("#bt_calc").click(function() {
     if (!checkData()) {
-      $("#err_field").html("Заполните все поля.")
+      $("#err_field").html("Заполните все поля.");
+      return;
     }
+    let points_plain;
+    points_plain = extractPoints(martix_inputs.slice(0, 3));
+    point_vec_AD = extractPoints([martix_inputs[0], martix_inputs[3]]);
+    getNormalOfPlain(points_plain[0], points_plain[1], points_plain[2]);
   });
 });
